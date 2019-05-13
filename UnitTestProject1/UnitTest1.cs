251663d70e754e9c3cd32fcdd938e51832972c49
Erkan Onğur <erkanongur@hotmail.com>
2019-05-13 03:35:35 +0300

    TestsAdded-13.05.19
    Unit Tests added and working..

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Ceasar_kontrol()
+        {
+            bilgiguvenligi.Form1 _proje = new bilgiguvenligi.Form1();
+            String test_sonuc = _proje.Ceasar("...", 1);
+            String test_beklenen = "///";
+            Assert.AreEqual(test_beklenen, test_sonuc);
+        }
+        [TestMethod]
+        public void Ceasar_SayiGiris_Kontrol()
+        {
+            bilgiguvenligi.Form1 _proje = new bilgiguvenligi.Form1();
+            String test_sonuc = _proje.Ceasar("123", 1);
+            String test_beklenen = "234";
+            Assert.AreNotEqual(test_beklenen, test_sonuc);
+        }
+        [TestMethod]
+        public void Ceasar_Decode_Kontrol()
+        {
+            bilgiguvenligi.Form1 _proje = new bilgiguvenligi.Form1();
+            String test_sonuc = _proje.Ceasar_Decode("///", 1);
+            String test_beklenen = "...";
+            Assert.AreEqual(test_beklenen, test_sonuc);
+        }
+        [TestMethod]
+        public void Cit_Kontrol()
+        {
+            bilgiguvenligi.Form1 _proje = new bilgiguvenligi.Form1();
+            String test_sonuc = _proje.Cit("1.2#3~");
+            String test_beklenen = "123.#~";
+            Assert.AreEqual(test_beklenen, test_sonuc);
+        }
+        [TestMethod]
+        public void Cit_Decode_Kontrol()
+        {
+            bilgiguvenligi.Form1 _proje = new bilgiguvenligi.Form1();
+            String test_sonuc = _proje.Cit_Decode("123.#~");
+            String test_beklenen = "1.2#3~";
+            Assert.AreEqual(test_beklenen, test_sonuc);
+        }
+        [TestMethod]
+        public void Sutun_Kontrol()
+        {
+            bilgiguvenligi.Form1 _proje = new bilgiguvenligi.Form1();
+            String test_sonuc = _proje.Sutun("E123R123K123A123N123", 4);
+            String test_beklenen = "ERKAN111112222233333";
+            Assert.AreEqual(test_beklenen, test_sonuc);
+        }
+        [TestMethod]
+        public void Sutun_SifirGiris_Kontrol()
+        {
+            bilgiguvenligi.Form1 _proje = new bilgiguvenligi.Form1();
+            String test_sonuc = _proje.Sutun("E123R123K123A123N123", 0);
+            String test_beklenen = "Sütun Sayısı 0'dan büyük olmalıdır.";
+            Assert.AreEqual(test_beklenen, test_sonuc);
+        }
+        [TestMethod]
+        public void Sutun_NegatifGiris_Kontrol()
+        {
+            bilgiguvenligi.Form1 _proje = new bilgiguvenligi.Form1();
+            String test_sonuc = _proje.Sutun("E123R123K123A123N123", -1);
+            String test_beklenen = "Sütun Sayısı 0'dan büyük olmalıdır.";
+            Assert.AreEqual(test_beklenen, test_sonuc);
+        }
+        [TestMethod]
+        public void Polybius_Kontrol_BuyukHarf()
+        {
+            bilgiguvenligi.Form1 _proje = new bilgiguvenligi.Form1();
+            //BÜYÜK HARF KONTROLÜ
+            String test2_sonuc = _proje.Polybius("ERKAN");
+            String test2_beklenen = "04 31 20 00 23";
+            Assert.AreEqual(test2_beklenen, test2_sonuc);
+        }
+        [TestMethod]
+        public void Polybius_Kontrol_KucukHarf()
+        {
+            bilgiguvenligi.Form1 _proje = new bilgiguvenligi.Form1();
+            //KÜÇÜK HARF KONTROLÜ
+            String test_sonuc = _proje.Polybius("erkan");
+            String test_beklenen = "04 31 20 00 23";
+            Assert.AreEqual(test_beklenen, test_sonuc);
+        }
+    }
+}
--- bilgiguvenligi/Form1.cs
+++ bilgiguvenligi/Form1.cs
@@ -16,254 +16,230 @@ namespace bilgiguvenligi
     {
         public Form1()
         { 
-       
-            InitializeComponent();
-            
+            InitializeComponent();  
         }
-
-        
-     
     private void Form1_Load(object sender, EventArgs e)
-        {
-           
+        {           
             comboBox1.Items.Add("Ceasar");
             comboBox1.Items.Add("Sütun");
             comboBox1.Items.Add("Çit");
-            comboBox1.Items.Add("MatrisYardımıyla");
             comboBox1.Items.Add("Polybius");
         }
+
 
         private void button1_Click(object sender, EventArgs e)
         {
             if(comboBox1.Text=="Ceasar")
             {
                 int anahtar = Convert.ToInt32(textBox3.Text);
-                string sifrelimetin = textBox2.Text;
-                string acikmetin = "";
-                char[] chrText = sifrelimetin.ToCharArray();
-                for (int i = 0; i < chrText.Length; i++)
-                {
-                    acikmetin = acikmetin + (char)(chrText[i] - anahtar);
-                }
-                textBox5.Text = acikmetin;
-            }
-            if(comboBox1.Text=="MatrisYardımıyla")
-            {
 
+                string sifrelimetin = textBox2.Text;
+
+                string acikmetin = Ceasar_Decode(sifrelimetin, anahtar);
+                
+                textBox5.Text = acikmetin;
             }
             if (comboBox1.Text == "Çit")
             {
                 string sifrelimetin = textBox2.Text;
-                string ilk = "", son = "",acikmetin="",xx="";
-                if (sifrelimetin.Length % 2 == 0)
-                {
-                    ilk = sifrelimetin.Substring(0, sifrelimetin.Length / 2);
-                    son = sifrelimetin.Substring(sifrelimetin.Length / 2);
-                    for (int i = 0; i < sifrelimetin.Length / 2; i++)
-                    {
-                        acikmetin += ilk[i].ToString() + son[i].ToString();
-                    }
-                    textBox5.Text = acikmetin;
-                }
-                else
-                {
-                    ilk = sifrelimetin.Substring(0, (sifrelimetin.Length / 2) + 1);
-                    son = sifrelimetin.Substring((sifrelimetin.Length / 2)+1);
-                    for (int i = 0; i < (sifrelimetin.Length / 2); i++)
-                    {
-                        acikmetin += ilk[i].ToString() + son[i].ToString();
-                    }
-                    for (int i = 0; i < ilk.Length; i++)
-                    {
-                         xx= ilk[i].ToString();
-                    }
-                    textBox5.Text = acikmetin+xx;
-                }
-            }
-            if (comboBox1.Text == "Sütun")
-            {
-
-            }
-            if (comboBox1.Text == "Polybius")
-            {
-
+                string acikmetin = Cit_Decode(sifrelimetin);
+                textBox5.Text = acikmetin;
             }
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            //if (String.IsNullOrEmpty(textBox3.Text)) { MessageBox.Show("Boş bırakılamaz!"); }
 
             if (comboBox1.Text=="Ceasar")
             {
                 int anahtar = Convert.ToInt32(textBox3.Text);
+
                 string acikmetin = textBox1.Text;
-                string sifrelimetin = "";
-                char[] chrText = acikmetin.ToCharArray();
-                for (int i = 0; i < chrText.Length; i++)
-                {
-                    sifrelimetin = sifrelimetin +(char)(chrText[i] + anahtar);
-                }
+                                
+                string sifrelimetin = Ceasar(acikmetin, anahtar);
+                
                 textBox2.Text = sifrelimetin;
             }
             if (comboBox1.Text == "Sütun")
             {
-                string sifrelimetin = "";
-                int sayac = 0;
-                Random rnd = new Random();
-                int sutunsayi = Convert.ToInt32(textBox4.Text);
                 string acikmetin1 = textBox1.Text;
-                string acikmetin = acikmetin1.Replace(" ","");
-                int matrisboy = 0;
-                if (acikmetin.Length % sutunsayi != 0)
-                {
-                    matrisboy = ((acikmetin.Length / sutunsayi) + 1) * sutunsayi;
-                }
-                int s1 = matrisboy - acikmetin.Length;
-                if (matrisboy > acikmetin.Length)
-                {
-                    for (int i = 0; i <s1; i++)
-                    {
-                        int kod = rnd.Next(97, 122);
-                        acikmetin +=Convert.ToChar(kod);
-                    }
-                }
+                string acikmetin = acikmetin1.Replace(" ", "");
+                int sutunsayi = Convert.ToInt32(textBox4.Text);
 
-                char[] chrText = acikmetin.ToCharArray();
-                char[,] matris = new char[chrText.Length / sutunsayi, sutunsayi];
-        
-                
-                for (int i = 0; i < chrText.Length / sutunsayi; i++)
-                {
-                    for (int j = 0; j < sutunsayi; j++)
-                    {
-                        matris[i, j] = chrText[sayac];
-                        sayac++;
-                    }
-                }
-                for (int i = 0; i <  sutunsayi; i++)
-                {
-                    for (int j = 0; j <chrText.Length/ sutunsayi; j++)
-                    {
-                        sifrelimetin+=matris[j, i];
-                    }
-                }
+                string sifrelimetin = Sutun(acikmetin, sutunsayi);
+
                 textBox2.Text = sifrelimetin;
+
+                
             }
             if (comboBox1.Text == "Çit")
             {
                 string acikmetin = textBox1.Text;
-                char[] chrText = acikmetin.ToCharArray();
-                string tek = "";
-                string cift = "";
-                    for (int i = 0; i < chrText.Length; i += 2)
-                    {
-                        tek += chrText[i].ToString();
-                    }
-                    for (int i = 1; i < chrText.Length; i = i + 2)
-                    {
-                        cift += chrText[i].ToString();
-                    }
-                    textBox2.Text = tek.ToString() + cift.ToString(); 
-            }
-            if (comboBox1.Text == "MatrisYardımıyla")
-            {
-                string metin1 = textBox1.Text;
-                string metin = metin1.Replace(" ", "0");
-                int matrisboy = 0;
-                if (metin.Length % 3 != 0)
-                {
-                    matrisboy = (((metin.Length / 3) + 1) * 3);
-                }
-                int s1 = matrisboy - metin.Length;
-                if (matrisboy > metin.Length)
-                {
-                    for (int i = 0; i < s1; i++)
-                    {
-                        metin += 0;
-                    }
-                }
-
-                int[] alfabeindis = new int[metin.Length];
-                for (int i = 0; i < metin.Length; i++)
-                {
-                    alfabeindis[i] = metin[i];
-                    if (alfabeindis[i] == 48)
-                        alfabeindis[i] = 0;
-                }
-                int sayac = 0;
-                int[,] matris = new int[3, (alfabeindis.Length) / 3];
-                for (int i = 0; i < 3; i++)
-                {
-                    for (int j = 0; j < (alfabeindis.Length) / 3; j++)
-                    {
-                        matris[i, j] = alfabeindis[sayac];
-                        sayac++;
-                    }
-                }
-                int[,] m = new int[3, 3] { { 3, 2, 2 }, { 0, 1, 0 }, { 1, 0, 1 } };
-                int[,] sifreli = new int[m.GetUpperBound(0)+1,matris.GetUpperBound(1)+1];
-                for (int i = 0; i <= m.GetUpperBound(0); i++)
-                {
-                    for (int j = 0; j <= matris.GetUpperBound(1); j++)
-                    {
-                        int toplam = 0;
-                        for (int k = 0; k <= m.GetUpperBound(1); k++)
-                        {
-                            toplam += m[i, k] * matris[k, j];
-                        }
-                        sifreli[i, j] = toplam;
-                    }
-                }
-                string sifrelimetin = "";
-                for (int i = 0; i < m.GetUpperBound(0) + 1; i++)
-                {
-                    for (int j = 0; j < matris.GetUpperBound(1) + 1; j++)
-                    {
-                        sifrelimetin += sifreli[i, j].ToString()+"\t";
-                        sifrelimetin += " ";
-                    }
-                    sifrelimetin += Environment.NewLine;
-                }
-                textBox2.Text = sifrelimetin;
+                String sifrelimetin = Cit(acikmetin);
+                textBox2.Text = sifrelimetin; 
             }
             if (comboBox1.Text == "Polybius")
             {
                 string acikmetin = textBox1.Text;
-                string acikmetin1 = acikmetin.Replace("ç", "c").Replace("ö", "o").Replace("ü","u").Replace("ı","i");
-                char[] chrText = acikmetin1.ToCharArray();
-                string[] sifrelimetin = new string[2 * (chrText.Length)];
-                string sonuc = "";
-                int sayac = 0;
-                char[,] matrisalfabe = new char[5, 5] { { 'a', 'b', 'c', 'd', 'e' },{'f','g','h','i','j' },
+
+                String sifrelimetin = Polybius(acikmetin);
+
+                textBox2.Text = sifrelimetin;
+            }
+        }
+        //Fonksiyonlar
+
+        public String Ceasar(String acikmetin, int anahtar)
+        {
+            String sifrelimetin = "";
+            char[] chrText = acikmetin.ToCharArray();
+            for (int i = 0; i < chrText.Length; i++)
+            {
+                sifrelimetin = sifrelimetin + (char)(chrText[i] + anahtar);
+            }
+
+            return sifrelimetin;
+        }
+        public String Ceasar_Decode(String sifrelimetin, int anahtar)
+        {
+            String acikmetin = "";
+            char[] chrText = sifrelimetin.ToCharArray();
+            for (int i = 0; i < chrText.Length; i++)
+            {
+                acikmetin = acikmetin + (char)(chrText[i] - anahtar);
+            }
+            return acikmetin;
+        }
+
+        public String Cit(String acikmetin)
+        {
+            char[] chrText = acikmetin.ToCharArray();
+            string tek = "";
+            string cift = "";
+            for (int i = 0; i < chrText.Length; i += 2)
+            {
+                tek += chrText[i].ToString();
+            }
+            for (int i = 1; i < chrText.Length; i = i + 2)
+            {
+                cift += chrText[i].ToString();
+            }
+            String sifrelimetin = tek.ToString() + cift.ToString();
+
+            return sifrelimetin;
+        }
+        public String Cit_Decode(String sifrelimetin)
+        {
+            string ilk = "", son = "", acikmetin = "", xx = "";
+            if (sifrelimetin.Length % 2 == 0)
+            {
+                ilk = sifrelimetin.Substring(0, sifrelimetin.Length / 2);
+                son = sifrelimetin.Substring(sifrelimetin.Length / 2);
+                for (int i = 0; i < sifrelimetin.Length / 2; i++)
+                {
+                    acikmetin += ilk[i].ToString() + son[i].ToString();
+                }
+                return acikmetin;
+            }
+            else
+            {
+                ilk = sifrelimetin.Substring(0, (sifrelimetin.Length / 2) + 1);
+                son = sifrelimetin.Substring((sifrelimetin.Length / 2) + 1);
+                for (int i = 0; i < (sifrelimetin.Length / 2); i++)
+                {
+                    acikmetin += ilk[i].ToString() + son[i].ToString();
+                }
+                for (int i = 0; i < ilk.Length; i++)
+                {
+                    xx = ilk[i].ToString();
+                }
+                return acikmetin + xx;
+            }
+        }
+        public String Sutun(String acikmetin, int sutunsayi)
+        {
+
+            string sifrelimetin = "";
+            int sayac = 0;
+            Random rnd = new Random();
+
+            if(sutunsayi <= 0)
+            {
+                sifrelimetin = "Sütun Sayısı 0'dan büyük olmalıdır.";
+                return sifrelimetin;
+            }
+            int matrisboy = 0;
+            if (acikmetin.Length % sutunsayi != 0)
+            {
+                matrisboy = ((acikmetin.Length / sutunsayi) + 1) * sutunsayi;
+            }
+            int s1 = matrisboy - acikmetin.Length;
+            if (matrisboy > acikmetin.Length)
+            {
+                for (int i = 0; i < s1; i++)
+                {
+                    int kod = rnd.Next(97, 122);
+                    acikmetin += Convert.ToChar(kod);
+                }
+            }
+
+            char[] chrText = acikmetin.ToCharArray();
+            char[,] matris = new char[chrText.Length / sutunsayi, sutunsayi];
+
+
+            for (int i = 0; i < chrText.Length / sutunsayi; i++)
+            {
+                for (int j = 0; j < sutunsayi; j++)
+                {
+                    matris[i, j] = chrText[sayac];
+                    sayac++;
+                }
+            }
+            for (int i = 0; i < sutunsayi; i++)
+            {
+                for (int j = 0; j < chrText.Length / sutunsayi; j++)
+                {
+                    sifrelimetin += matris[j, i];
+                }
+            }
+            return sifrelimetin;
+        }
+        public String Polybius(String acikmetin)
+        {
+            string acikmetin1 = acikmetin.Replace("ç", "c").Replace("ö", "o").Replace("ü", "u").Replace("ı", "i");
+
+            char[] chrText = acikmetin1.ToCharArray();
+
+            string[] sifrelimetin = new string[2 * (chrText.Length)];
+
+            string sonuc = "";
+
+            int sayac = 0;
+
+            char[,] matrisalfabe = new char[5, 5] { { 'a', 'b', 'c', 'd', 'e' },{'f','g','h','i','j' },
                                                 {'k','l','m','n','o'},{'p','r','s','ş','t' },
                                                 { 'u','ü','v','y','z' } };
-
-                
-                for (int k = 0; k < chrText.Length; k++)
+            for (int k = 0; k < chrText.Length; k++)
+            {
+                for (int i = 0; i < 5; i++)
                 {
-                    for (int i = 0; i < 5; i++)
+                    for (int j = 0; j < 5; j++)
                     {
-                        for (int j = 0; j < 5; j++)
+                        if (matrisalfabe[i, j] == chrText[k])
                         {
-                            if(matrisalfabe[i,j]==chrText[k])
-                            {
-
-                                sifrelimetin[sayac] = Convert.ToString(i)+Convert.ToString(j);
-                                sayac++;
-                            }
+                            sifrelimetin[sayac] = Convert.ToString(i) + Convert.ToString(j);
+                            sayac++;
                         }
                     }
                 }
-                for (int i = 0; i < 2*(chrText.Length); i++)
-                {
-                    sonuc = sonuc + sifrelimetin[i]+" ";
-                }
-                textBox2.Text = sonuc;
-
             }
+            for (int i = 0; i < 2 * (chrText.Length); i++)
+            {
+                sonuc = sonuc + sifrelimetin[i] + " ";
+            }
+            sonuc = sonuc.Trim();
+            return sonuc;    
         }
-
         private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
         {
             e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
@@ -271,7 +247,6 @@ namespace bilgiguvenligi
 
         private void textBox4_KeyPress(object sender, KeyPressEventArgs e)
         {
-            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
         }
 
         private void Form1_Shown(object sender, EventArgs e)
